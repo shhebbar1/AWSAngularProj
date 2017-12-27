0645ff4e76701bd5d5a12bacf1cb5bbc695be863
Subramanya Hebbar <shhebbar1@gmail.com>
2017-12-27 16:02:31 -0600

    Folder changes

--- client/my-app/src/app/services/hero.service.ts
+++ client/my-app/src/app/services/hero.service.ts
@@ -3,8 +3,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 
-import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 import { MessageService } from './message.service';
 
 @Injectable()
