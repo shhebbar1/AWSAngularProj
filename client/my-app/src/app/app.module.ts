c7c9e609d01c44c299a5f38c2de9eb0d7b287150
Subramanya Hebbar <shhebbar1@gmail.com>
2017-12-27 15:54:03 -0600

    Services commit

--- client/my-app/src/app/app.module.ts
+++ client/my-app/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
